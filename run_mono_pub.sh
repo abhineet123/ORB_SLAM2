aac3c9787403534cdb5d9f9b49f9a17281833f20
gaunthan <gaunthanhuang@foxmail.com>
2019-05-08 10:20:59 +0800

    Support for running as ros node

--- run_mono_pub.sh
+++ run_mono_pub.sh
@@ -2,6 +2,21 @@
 
 export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:`pwd`/Examples/ROS
 
-rosrun ORB_SLAM2 Monopub Vocabulary/ORBvoc.txt \
-  Examples/Monocular/KITTI00-02.yaml \
-  /home/${USER}/Documents/dataset/KITTI/2011_09_26/2011_09_26_drive_0002_sync/image_00
+print_help()
+{
+  echo -e "Usage:\n\t$0 path_to_camera_settings path_to_sequence/camera_id/[-1 <image_topic>]"
+  echo -e "\nExamples:"
+  echo -e "\n\t$0 Examples/Monocular/KITTI00-02.yaml /home/${USER}/Documents/dataset/KITTI/raw_data/2011_09_26/2011_09_26_drive_0002_sync/image_00"
+  echo -e "\n\t$0 Examples/RGB-D/kinect.yaml -1 /camera/rgb/image_raw"
+}
+
+if [ "$#" -lt "2" ]; then
+  print_help
+  exit 0
+fi
+
+#rosrun ORB_SLAM2 Monopub Vocabulary/ORBvoc.txt \
+#  Examples/Monocular/KITTI00-02.yaml \
+#  /home/${USER}/Documents/dataset/KITTI/raw_data/2011_09_26/2011_09_26_drive_0002_sync/image_00
+
+rosrun ORB_SLAM2 Monopub Vocabulary/ORBvoc.txt $1 $2 $3
