c9ad273569fe1d91e77c87be03a85a8239bc5653
gaunthan <gaunthanhuang@foxmail.com>
2019-05-30 10:11:10 +0800

    Use another example

--- run_mono_pub.sh
+++ run_mono_pub.sh
@@ -6,7 +6,7 @@ print_help()
 {
   echo -e "Usage:\n\t$0 path_to_camera_settings path_to_sequence/camera_id/[-1 <image_topic>]"
   echo -e "\nExamples:"
-  echo -e "\n\t$0 Examples/Monocular/KITTI00-02.yaml /home/${USER}/Documents/dataset/KITTI/raw_data/2011_09_26/2011_09_26_drive_0002_sync/image_00"
+  echo -e "\n\t$0 Examples/Monocular/KITTI03.yaml /home/kerry/Documents/dataset/KITTI/raw_data/2011_10_03/2011_10_03_drive_0027_sync/image_00/"
   echo -e "\n\t$0 Examples/RGB-D/kinect.yaml -1 /camera/rgb/image_raw"
 }
 
@@ -15,8 +15,5 @@ if [ "$#" -lt "2" ]; then
   exit 0
 fi
 
-#rosrun ORB_SLAM2 Monopub Vocabulary/ORBvoc.txt \
-#  Examples/Monocular/KITTI00-02.yaml \
-#  /home/${USER}/Documents/dataset/KITTI/raw_data/2011_09_26/2011_09_26_drive_0002_sync/image_00
 
 rosrun ORB_SLAM2 Monopub Vocabulary/ORBvoc.txt $1 $2 $3
