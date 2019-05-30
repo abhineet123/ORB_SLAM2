2c93af526806177d238f00598d6d663c8721e1d5
gaunthan <gaunthanhuang@foxmail.com>
2019-05-30 15:24:08 +0800

    Use indoor configuration

--- run_mono_sub.sh
+++ run_mono_sub.sh
@@ -3,4 +3,7 @@
 export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:`pwd`/Examples/ROS
 
 # Start subscriber on KITTI 00
-rosrun ORB_SLAM2 Monosub 5 3 29 -25 48 -12 0.55 0.50 1 5
+#rosrun ORB_SLAM2 Monosub 5 3 29 -25 48 -12 0.55 0.50 1 5
+
+# Start subscriber on indoor scene
+rosrun ORB_SLAM2 Monosub 30 2 5 -5 5 -5 0.55 0.50 1 5
