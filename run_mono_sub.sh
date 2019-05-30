73c92b3cf56db13a47cf67130ffca16fbd0083df
gaunthan <gaunthanhuang@foxmail.com>
2019-05-30 17:01:59 +0800

    Read map setting from command line

--- run_mono_sub.sh
+++ run_mono_sub.sh
@@ -2,8 +2,17 @@
 
 export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:`pwd`/Examples/ROS
 
-# Start subscriber on KITTI 00
-#rosrun ORB_SLAM2 Monosub 5 3 29 -25 48 -12 0.55 0.50 1 5
+print_help()
+{
+  echo -e "Usage:\n\t$0 scale_factor resize_factor cloud_max_x cloud_min_x cloud_max_z cloud_min_z free_thresh occupied_thresh"
+  echo -e "\nExamples:"
+  echo -e "\t$0 5 3 29 -25 48 -12 0.55 0.50 1 5 (For outdoor scene)"
+  echo -e "\t$0 30 2 5 -5 5 -5 0.55 0.50 1 5 (For indoor scene)"
+}
 
-# Start subscriber on indoor scene
-rosrun ORB_SLAM2 Monosub 30 2 5 -5 5 -5 0.55 0.50 1 5
+if [ "$#" -lt "10" ]; then
+  print_help
+  exit 0
+fi
+
+rosrun ORB_SLAM2 Monosub $@
